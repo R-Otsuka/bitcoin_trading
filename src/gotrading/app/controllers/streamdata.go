3806a8ea4b2539f012f5ff8afc0c86b06f82cd79
kamio06 <yakugozy@gmail.com>
2020-10-10 17:49:00 +0900

    add-ajaxjaxx-method

--- src/gotrading/app/controllers/streamdata.go
+++ src/gotrading/app/controllers/streamdata.go
@@ -5,20 +5,9 @@ import (
 	"gotrading/bitflyer"
 	"gotrading/config"
 	"log"
-
-	"fmt"
 )
 
 func StreamIngestionData() {
-	limit := 100
-	duration := "1m"
-	durationTime := config.Config.Durations[duration]
-	df, _ := models.GetAllCandle(config.Config.ProductCode, durationTime, limit)
-	log.Printf("df=%v",df)
-	for i:=0;i<len(df.Candles);i++{
-		//log.Printf("dfCandle=%v\n",df.Candles[i])
-		log.Printf("%+v\n", df.Candles[i])
-	}
 	var tickerChannl = make(chan bitflyer.Ticker)
 	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
 	go apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChannl)
@@ -27,7 +16,6 @@ func StreamIngestionData() {
 			log.Printf("action=StreamIngestionData, %v", ticker)
 			for _, duration := range config.Config.Durations {
 				isCreated := models.CreateCandleWithDuration(ticker, ticker.ProductCode, duration)
-				fmt.Println(isCreated)
 				if isCreated == true && duration == config.Config.TradeDuration {
 					// TODO
 				}
